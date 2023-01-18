2ddfa8eafb5e4d90ee16869cfc2c368c11ce8153
MaciejZzadupia <zelek.maciej@gmail.com>
2023-01-18 13:54:14 +0000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿// var myAge = 33;
+int myAge = 120;
+int newAge = myAge + 5;
+Console.WriteLine(newAge);
+int myVar = int.MinValue;
+uint myVar2 = uint.MaxValue;
+long myVar3 = long.MaxValue;
+ulong myVar4 = ulong.MaxValue;
+
+
+// zmiennoprzecinkowe
+float myNumber = float.MaxValue;
+double myNumber2 = double.MaxValue;
+
+// zmienne tekstowe
+string name = "Adam";
+string surname = "Kamizelich";
+string result = name + surname;
+Console.WriteLine(result);
+char myVar5 = 'c';
+
+var result2 = name.ToArray();
+// zmienna logiczna
+bool isActive = true;
+var isValid = 5 > 6;
+Console.WriteLine(isValid);
+
+
